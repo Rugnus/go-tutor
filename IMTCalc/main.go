ece1069da230e5f1acac1b8055b1f69c64bbeafe
SYGasanov <sygasanov@get-lab.ru>
2024-09-25 17:33:44 +0300

    first commit

---
+++ IMTCalc/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	const IMTPower = 2
+	var userHeight float64
+	var userKg float64
+	fmt.Println("__ Калькулятор индекса массы тела __")
+	fmt.Print("Введите свой рост в сантиметрах: ")
+	fmt.Scan(&userHeight)
+	fmt.Print("Введите свой вес: ")
+	fmt.Scan(&userKg)
+	IMT := userKg / math.Pow(userHeight/100, IMTPower)
+	fmt.Printf("Ваш инедекс массы тела: %.1f", IMT)
+}

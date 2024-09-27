45f3651ba857e1cfc73de5ed6e266eb3b3103712
SYGasanov <sygasanov@get-lab.ru>
2024-09-27 15:41:25 +0300

    optimize code

--- IMTCalc/main.go
+++ IMTCalc/main.go
@@ -9,11 +9,13 @@ func main() {
 	const IMTPower = 2
 	var userHeight float64
 	var userKg float64
-	fmt.Println("__ Калькулятор индекса массы тела __")
-	fmt.Print("Введите свой рост в сантиметрах: ")
+	fmt.Print(`__ Калькулятор индекса массы тела __
+Введите свой рост в сантиметрах: `)
+
 	fmt.Scan(&userHeight)
 	fmt.Print("Введите свой вес: ")
 	fmt.Scan(&userKg)
 	IMT := userKg / math.Pow(userHeight/100, IMTPower)
-	fmt.Printf("Ваш инедекс массы тела: %.1f", IMT)
+	result := fmt.Sprintf("Ваш инедекс массы тела: %.1f", IMT)
+	fmt.Println(result)
 }

3059c0dc857b91e8e32d7d802641f2e331ca5a35
SYGasanov <sygasanov@get-lab.ru>
2024-09-30 12:20:10 +0300

    optimize conde using func and switch

--- IMTCalc/main.go
+++ IMTCalc/main.go
@@ -6,7 +6,27 @@ import (
 )
 
 func main() {
-	const IMTPower = 2
+
+	userHeight, userKg := getUserInput()
+	IMT := calculateIMT(userHeight, userKg)
+	outputResult(IMT)
+
+	switch {
+	case IMT < 16:
+		fmt.Println("У вас выраженный дефицит массы")
+	case IMT < 18.5:
+		fmt.Println("У вас недостаточная масса тела")
+	case IMT < 25:
+		fmt.Println("У вас нормальный вес")
+	case IMT < 30:
+		fmt.Println("У вас избыточная масса тела")
+	default:
+		fmt.Println("У вас ожирение")
+	}
+}
+
+func getUserInput() (float64, float64) {
+
 	var userHeight float64
 	var userKg float64
 	fmt.Print(`__ Калькулятор индекса массы тела __
@@ -15,7 +35,15 @@ func main() {
 	fmt.Scan(&userHeight)
 	fmt.Print("Введите свой вес: ")
 	fmt.Scan(&userKg)
-	IMT := userKg / math.Pow(userHeight/100, IMTPower)
+	return userHeight, userKg
+}
+
+func calculateIMT(userHeight float64, userKg float64) float64 {
+	IMT := userKg / math.Pow(userHeight/100, 2)
+	return IMT
+}
+
+func outputResult(IMT float64) {
 	result := fmt.Sprintf("Ваш инедекс массы тела: %.1f", IMT)
 	fmt.Println(result)
 }
